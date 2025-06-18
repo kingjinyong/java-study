9881f97b38e3d8ab4320bf232519976f44722263
kingjinyong <wlsdydwhswkf@gmail.com>
2025-06-18 17:45:07 +0900

    별찍기 코드 수정

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,44 @@
 public class Main {
 
-    //         *
+    public static void main(String[] args) {
+//        *****
+//        *****
+//        *****
+//        *****
+//        *****
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+//    *     *
+//     *   *
+//       *
+//     *   *
+//    *     *
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (i == j || i + j == 4) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+//         *
 //        **
 //       ***
 //      ****
 //     *****
-    public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
 
             for (int j = 0; j < 5 - (i + 1); j++) {
@@ -63,24 +96,13 @@ public class Main {
 //          *
 //         ***
 //        *****
-        int n = 5;
-        int mid = n / 2;
-        for (int i = 0; i <= mid; i++) {
-            for (int j = 0; j < i; j++) {
-                System.out.print(" ");
-            }
-            for (int j = 0; j < n - 2 * i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-
-        for (int i = mid + 1; i < n; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                System.out.print(" ");
-            }
-            for (int j = 0; j < n - 2 * (n - i - 1); j++) {
-                System.out.print("*");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if ((i >= j && i + j >= 4) || (i <= j && i + j <= 4)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
@@ -92,33 +114,20 @@ public class Main {
 //        *****
 //        ** **
 //        *   *
-        int N = 5;
-        for (int i = 0; i < N; i++) {
-            if (i == N / 2) {
-                for (int j = 0; j < N; j++) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (!((i > j && i + j > 4) || (i < j && i + j < 4))) {
                     System.out.print("*");
-                }
-            } else {
-                int star, space;
-                if (i < N / 2) {
-                    star = i + 1;
-                    space = N - 2 * star;
                 } else {
-                    star = N - i;
-                    space = N - 2 * star;
-                }
-
-                for (int j = 0; j < star; j++) {
-                    System.out.print("*");
-                }
-                for (int j = 0; j < space; j++) {
                     System.out.print(" ");
-                }
-                for (int j = 0; j < star; j++) {
-                    System.out.print("*");
                 }
             }
             System.out.println();
         }
+
+        System.out.println();
+
+        int number = 300;
+        System.out.println((byte) number);
     }
 }
