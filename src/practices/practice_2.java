61c1a9e87ad2f192a4d42f76f5df38de59db54d6
kingjinyong <wlsdydwhswkf@gmail.com>
2025-06-24 16:50:11 +0900

    실습 2-주석 설명

--- src/practices/practice_2.java
+++ src/practices/practice_2.java
@@ -7,6 +7,11 @@ public class practice_2 {
         int s = 0;
         int b = 0;
 
+        // arr과 arr2를 같이 순환 함.
+        // 동시에 같은 숫자가 나오면 s++
+        // 다 돌았는데도 해당 인덱스에서 동시에 같은 값이 나오지 못 하면
+        // 해당 인덱스 기준으로 다음 인덱스부터 끝 인덱스까지 검사
+        // 같은 숫자 나오면 b++
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == arr2[i]) {
                 s++;
