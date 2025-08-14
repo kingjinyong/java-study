5b985d30b5ab08e81cea53bf44268fafd3d096f9
kingjinyong <wlsdydwhswkf@gmail.com>
2025-08-14 18:02:34 +0900

    형변환 예제

---
+++ src/javajungseok/chapters/chapter2/CastingEx5.java
@@ -0,0 +1,13 @@
+package javajungseok.chapters.chapter2;
+
+public class CastingEx5 {
+    public static void main(String[] args) {
+        String str = "3";
+
+        System.out.println(str.charAt(0) - '0');
+        System.out.println('3' - '0' + 1);
+        System.out.println(Integer.parseInt("3") + 1);
+        System.out.println("3" + 1);
+        System.out.println((char) (3 + '0'));
+    }
+}
