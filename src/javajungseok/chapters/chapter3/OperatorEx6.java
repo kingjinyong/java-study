c5ed0eb9cf5aaec40eb1da132203bbad36535cd6
kingjinyong <wlsdydwhswkf@gmail.com>
2025-08-19 16:48:20 +0900

    연산자 예제 - 컴파일 에러

---
+++ src/javajungseok/chapters/chapter3/OperatorEx6.java
@@ -0,0 +1,12 @@
+package javajungseok.chapters.chapter3;
+
+public class OperatorEx6 {
+    public static void main(String[] args) {
+        byte a = 10;
+        byte b = 20;
+        byte c = a + b;
+        // a와 b는 int형보다 작은 byte형이기 때문에 연산자 '+'는 이 두개의 피연산자들의 자료형을 int 형으로 변환한 다음 연산(덧셈) 수행
+        // 그래서 byte에 저장하려고 했기 때문에 컴파일 에러
+        System.out.println(c);
+    }
+}
