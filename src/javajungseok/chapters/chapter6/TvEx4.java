c53c87f6c3580bc5c06411c685f5ea6e6be42b40
kingjinyong <wlsdydwhswkf@gmail.com>
2025-08-26 21:56:19 +0900

    예제 6-4

--- src/javajungseok/chapters/chapter6/TvEx4.java
+++ src/javajungseok/chapters/chapter6/TvEx4.java
@@ -1,21 +1,5 @@
 package javajungseok.chapters.chapter6;
 
-public class TvEx4 {
-    public static void main(String[] args) {
-        Tv4[] tvArr = new Tv4[3];
-
-        for (int i = 0; i < tvArr.length; i++) {
-            tvArr[i] = new Tv4();
-            tvArr[i].channel = i + 10;
-        }
-
-        for (int i = 0; i < tvArr.length; i++) {
-            tvArr[i].channelUp();
-            System.out.printf("tvArr[%d].channel=%d%n", i, tvArr[i].channel);
-        }
-    }
-}
-
 class Tv4 {
     String color;
     boolean power;
@@ -34,3 +18,21 @@ class Tv4 {
     }
 
 }
+
+public class TvEx4 {
+    public static void main(String[] args) {
+        Tv4[] tvArr = new Tv4[3]; // 길이가 3인 Tv객체 배열
+
+        // Tv객체를 생성해서 Tv객체 배열의 각 요소에 저장
+        for (int i = 0; i < tvArr.length; i++) {
+            tvArr[i] = new Tv4();
+            tvArr[i].channel = i + 10; // tvArr[i]의 channel에 i + 10을 저장
+        }
+
+        for (int i = 0; i < tvArr.length; i++) {
+            tvArr[i].channelUp();   // tvArr[i] 의 메서드를 호출. 채널이 1증가
+            System.out.printf("tvArr[%d].channel=%d%n", i, tvArr[i].channel);
+        }
+    }
+}
+
