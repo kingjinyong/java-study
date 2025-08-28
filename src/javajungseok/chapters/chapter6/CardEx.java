ac1aa6a76dea8d4c77115e1caa45fa38030be9a5
kingjinyong <wlsdydwhswkf@gmail.com>
2025-08-28 09:25:55 +0900

    예제 6-5

--- src/javajungseok/chapters/chapter6/CardEx.java
+++ src/javajungseok/chapters/chapter6/CardEx.java
@@ -1,5 +1,13 @@
 package javajungseok.chapters.chapter6;
 
+
+class Card {
+    String kind;
+    int number;
+    static int width = 100;
+    static int height = 250;
+}
+
 public class CardEx {
     public static void main(String[] args) {
         System.out.println("Card.width = " + Card.width);
@@ -10,28 +18,18 @@ public class CardEx {
         c1.number = 7;
 
         Card c2 = new Card();
-        c2.kind = "Space";
+        c2.kind = "Spade";
         c2.number = 4;
+
         System.out.println("c1은 " + c1.kind + ", " + c1.number + "이며, 크기는 (" + c1.width + ", " + c1.height + ")");
         System.out.println("c2은 " + c2.kind + ", " + c2.number + "이며, 크기는 (" + c2.width + ", " + c2.height + ")");
 
-        System.out.println("c1의 width와 height를 각각 50, 80으로 변경합니다.");
-
-        // Card.width, Card.height로 하는 것을 권장
-//        c1.width = 50;
-//        c1.height = 80;
-        Card.width = 50;
-        Card.height = 80;
+        System.out.println("c1의  width와 height를 각각 50, 80으로 변경");
+        c1.width = 50;
+        c1.height = 80;
 
         System.out.println("c1은 " + c1.kind + ", " + c1.number + "이며, 크기는 (" + c1.width + ", " + c1.height + ")");
         System.out.println("c2은 " + c2.kind + ", " + c2.number + "이며, 크기는 (" + c2.width + ", " + c2.height + ")");
     }
 
-}
-
-class Card {
-    String kind;
-    int number;
-    static int width = 100;
-    static int height = 250;
 }
