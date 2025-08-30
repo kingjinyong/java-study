fc833db01deac3f4ab0dbd0a309056030ffabfc2
kingjinyong <wlsdydwhswkf@gmail.com>
2025-08-30 11:21:18 +0900

    예제 6-6

--- src/javajungseok/chapters/chapter6/MyMathEx.java
+++ src/javajungseok/chapters/chapter6/MyMathEx.java
@@ -4,13 +4,12 @@ public class MyMathEx {
     public static void main(String[] args) {
         MyMath mm = new MyMath();
         long result1 = mm.add(5L, 3L);
-
-        long result2 = mm.subtract(5L, 3L);
+        long result2 = mm.substract(5L, 3L);
         long result3 = mm.multiply(5L, 3L);
         double result4 = mm.divide(5L, 3L);
 
         System.out.println("add(5L, 3L) = " + result1);
-        System.out.println("subtract(5L, 3L) = " + result2);
+        System.out.println("substract(5L, 3L) = " + result2);
         System.out.println("multiply(5L, 3L) = " + result3);
         System.out.println("divide(5L, 3L) = " + result4);
     }
@@ -20,9 +19,10 @@ class MyMath {
     long add(long a, long b) {
         long result = a + b;
         return result;
+        // return a + b; // 위 의 두 줄을 한 줄로 간단히 할 수 있음
     }
 
-    long subtract(long a, long b) {
+    long substract(long a, long b) {
         return a - b;
     }
 
