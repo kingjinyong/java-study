57b9a0414880da4bbed49dbc7c713bb48d64651b
kingjinyong <wlsdydwhswkf@gmail.com>
2025-10-14 16:17:09 +0900

    예제 7-6

---
+++ src/javajungseok/chapters/chapter7/SuperEx2.java
@@ -0,0 +1,22 @@
+package javajungseok.chapters.chapter7;
+
+public class SuperEx2 {
+    public static void main(String[] args) {
+        Child2 c = new Child2();
+        c.method();
+    }
+}
+
+class Parent2 {
+    int x = 10;
+}
+
+class Child2 extends Parent {
+    int x = 20;
+
+    void method() {
+        System.out.println("x=" + x);
+        System.out.println("this.x=" + this.x);
+        System.out.println("super.x=" + super.x);
+    }
+}
