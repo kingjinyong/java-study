8f64726ec21b7f3bfc12038f712c962167ba87c3
kingjinyong <wlsdydwhswkf@gmail.com>
2025-10-14 15:46:07 +0900

    예제7-5

---
+++ src/javajungseok/chapters/chapter7/SuperEx.java
@@ -0,0 +1,20 @@
+package javajungseok.chapters.chapter7;
+
+public class SuperEx {
+    public static void main(String arsg[]) {
+        Child c = new Child();
+        c.method();
+    }
+}
+
+class Parent {
+    int x = 10;
+}
+
+class Child extends Parent {
+    void method() {
+        System.out.println("x=" + x);
+        System.out.println("this.x=" + this.x);
+        System.out.println("super.x=" + super.x);
+    }
+}
