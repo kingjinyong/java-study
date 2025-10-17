9fb32210e670549da344babf0bc900eca1479346
kingjinyong <wlsdydwhswkf@gmail.com>
2025-10-17 15:51:31 +0900

    예제 7-33

---
+++ src/javajungseok/chapters/chapter7/InnerEx.java
@@ -0,0 +1,27 @@
+package javajungseok.chapters.chapter7;
+
+public class InnerEx {
+    class InstanceInner {
+        int iv = 100;
+        static int cv = 100;          // 에러! static 변수를 선언할 수 없음.(JDK 16부터 허용)
+        final static int CONST = 100;   // final static은 상수이므로 허용
+    }
+
+    static class StaticInner {
+        int iv = 200;
+        static int cv = 200;        // static 클래스는 static 멤버를 정의할 수 있다.
+    }
+
+    void myMethod() {
+        class LocalInner {
+            int iv = 300;
+            static int cv = 300;          // 에러! static 변수를 선언할 수 없다.
+            final static int CONST = 300;   // final static은 상수이므로 허용
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(InstanceInner.CONST);
+        System.out.println(StaticInner.cv);
+    }
+}
